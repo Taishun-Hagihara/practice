4f37eace79c18df136885b9b1d1a945856c49644
Taishun-Hagihara <hagitaishun@gmail.com>
2025-06-09 17:02:41 +0900

    About async
    理解が浅い様な気がしているが、とりあえず完成させた

---
+++ taishun_async/script.js
@@ -0,0 +1,23 @@
+async function fetchUser() {
+  try{
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+
+    if(!response.ok) {
+      throw new Error("レスポンスの取得に失敗しました。");
+    }
+
+    const users = await response.json();
+    const userList = document.getElementById("user-list");
+    users.forEach(user => {
+      const li = document.createElement("li");
+      li.textContent = `${user.name} (${user.email})`;
+      userList.appendChild(li);
+    })
+
+  }catch {
+
+    const errorMessage = document.getElementById("error-message");
+    errorMessage.textContent = "データの取得に失敗しました：" + error.message;
+  }
+}
+fetchUser();
